758fbdf8a5428c601bf67f08167f10fff5defc6b
Raju Moni Borah <raju42fun@gmail.com>
2020-01-26 22:50:09 +0530

    updated to 5.4 completed with seeders

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,7 +15,57 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
+        'is_active'=>1,
+        'role_id'=>$faker->numberBetween(1,3),
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+//for post
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'category_id' => $faker->numberBetween(1,10),
+        'photo_id' => 1,
+        'title' => $faker->sentence(7,11),
+        'body' => $faker->paragraph(rand(10,15),true),
+    ];
+});
+//for role
+$factory->define(App\Role::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->randomElement(['administrator','author','subscriber'])
+    ];
+});
+//for category
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->randomElement(['php','java','bootstrap'])
+    ];
+});
+//for photo
+$factory->define(App\Photo::class, function (Faker\Generator $faker) {
+    return [
+        'file' => 'placeholder.jpg'
+    ];
+});
+//for comments
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'post_id' => $faker->numberBetween(1,10),
+        'is_active' => 1,
+        'author' => $faker->name,
+        'photo' => 'placeholder.jpg ',
+        'email' => $faker->safeEmail,
+        'body' => $faker->paragraph(1,true),
+    ];
+});
+//for commentreplies
+$factory->define(App\CommentReply::class, function (Faker\Generator $faker) {
+    return [
+        'is_active' => 1,
+        'author' => $faker->name,
+        'photo' => 'placeholder.jpg ',
+        'email' => $faker->safeEmail,
+        'body' => $faker->paragraph(1,true),
     ];
 });
